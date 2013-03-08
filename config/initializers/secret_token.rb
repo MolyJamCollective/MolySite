32d116d8e8943651785afa7eb03d1320f044c9af
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-03-08 07:47:02 -0500

    Init Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MolySite::Application.config.secret_token = '73f84a513947e825c3709a7b97b0bb19c2e7a5549c2075d18585d1da2b40feb69049c025fb3a1dca6ef45a8fac5b6ff890adf0fd226a35f5324a522f16c04063'
