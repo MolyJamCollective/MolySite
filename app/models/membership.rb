c7e310f7f883d18f81706e58a94f1581773db064
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-03-11 13:57:59 -0400

    Add Default Membership Rank

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,8 @@ class Membership < ActiveRecord::Base
   	attr_accessible :group_id, :role, :user_id
 
   	ROLES = { founder: 90, officer: 60, member: 30, }
+
+  	after_initialize do
+  		self.role = ROLES[:member]
+  	end
 end
