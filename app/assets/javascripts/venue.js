ebc0ac263a88622c947fccc2115475c534b723b4
Angus Perkerson <angusp@angusp.com>
2013-06-03 23:10:54 -0700

    Adds Location Index page, ability to see all filtered locations

--- app/assets/javascripts/venue.js
+++ app/assets/javascripts/venue.js
@@ -1,13 +1,13 @@
 /*
-* Location Sorting
-*/
-
+ * Location Sorting
+ */
 var options = {
-    valueNames: [ 'name', 'description', 'category', 'country' ]
+  valueNames: ['name', 'description', 'category', 'country']
 };
 
 var featureList = new List('locations-list', options);
 
+/*
 $('#filter-northamerica').click(function() {
     featureList.filter(function(item) {
         if (item.values().category == "northamerica") {
@@ -66,54 +66,124 @@ $('#filter-asia').click(function() {
 $('#filter-none').click(function() {
     featureList.filter();
     return false;
-});
+}); */
 
-var molydeux = (function(md, $) {
-  
-  function Location () {
-    
-    this.init = function() {
-      
-      $('.map').gmap3({
-        map: {
-          options: {
-                maxZoom: 14
-            }
-        },
-        
-        marker: {
-            values:[
-              {address:"San Francisco, US", data:"San Francisco, US"}
-            ],
-            options: {
-                icon: this.icon
-            }
+var molydeux = molydeux || {};
+
+molydeux.location = (function($) {
+
+  var that;
+
+    return {
+
+      init: function() {
+
+        that = molydeux.location;
+
+        $('.btn.filter')
+          .click(this.filterList);
+
+          that.updateListMap();
+
+      },
+
+      updateListMap: function() {
+
+        console.log($('#locations-list'));
+
+        if ($('#locations-list').length == 0)
+        {
+          console.log('no location list found');
+          return false;
         }
-      }, "autofit");
+        var locations = new Array();
 
-      
+        console.log("Locations: "+$('#locations-list .list li').length);
+
+        $('#locations-list .list li')
+          .each(function(index, element) {
+
+          locations.push({
+            address: $(element)
+              .find('.address')
+              .first()
+              .text(),
+            data: $(element)
+              .find('.name')
+              .first()
+              .text()
+          });
+
+        });
+
+        $('.map').gmap3('destroy').gmap3({
+          map: {
+            options: {
+              maxZoom: 14
+            }
+          },
+
+          marker: {
+            values: locations,
+            options: {
+              icon: this.icon
+            }
+          }
+        }, "autofit");
+
+      },
+
+      makeIcon: function(imagePath, width, height) {
+        var myIcon = null;
+
+        myIcon = new google.maps.MarkerImage(imagePath,
+        new google.maps.Size(width, height, "px", "px"));
+
+        return myIcon;
+
+      },
+
+      filterList: function(event) {
+
+        var target = event.target;
+
+        var category = 'none';
+
+        event.preventDefault();
+
+        if (!$(target)
+          .hasClass('filter')) {
+
+          target = $(target)
+            .parents('filter')
+            .first();
+
+        }
+
+        category = $(target)
+          .attr('data-category');
+
+        console.log('filter: ' + category);
+
+        featureList.filter(function(item) {
+          if (category == 'none') return true;
+
+          if ((item.values()
+            .category == category)) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+
+        that.updateListMap();
+
+      }
+
     };
-    
-    this.makeIcon = function(imagePath, width, height) {
-      var myIcon = null;
-      
-      myIcon = new google.maps.MarkerImage(imagePath, 
-        new google.maps.Size(width, height, "px", "px")
-      );
-      
-      return myIcon;
-      
-    };
-    
-  
-  };
-  
-  md.location = new Location();
-  
-  $(document).ready(function() {
-    md.location.init();
-  });
-  
-  return md;
-  
-}(molydeux || {}, jQuery));
+
+})(jQuery);
+
+$(document).ready(function() {
+  molydeux.location.init();
+});
