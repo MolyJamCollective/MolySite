0bca036fe9b67660f79dc2acbfd102921ecd73c5
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-06-06 13:01:34 -0400

    add username to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,7 @@ class User < ActiveRecord::Base
 
   	# Setup accessible (or protected) attributes for your model
   	attr_accessible :email, :password, :password_confirmation, :remember_me
-  	# attr_accessible :title, :body
-
-  	attr_accessible :name, :biography, :location, :website, :twitter_username
+  	attr_accessible :username, :name, :biography, :location, :website, :twitter_username
 
   def group?(group)
     self.groups.exists?(name: group.to_s)
