68bd36fb293d795dc42511f0784844f7fd607713
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-06-10 21:05:08 -0400

    make unauthorized access redirect to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   check_authorization :unless => :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-  	redirect_to '/', :alert => exception.message
+  	redirect_to '/login', :alert => exception.message
   end
 end
