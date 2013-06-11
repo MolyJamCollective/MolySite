b22f8c0af495e22c7e3d6d42abd2b66aec83f6ec
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-06-11 10:55:04 -0400

    make login redirect to previous location, close #37

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :unless => :devise_controller?
+  after_filter :store_location
 
   rescue_from CanCan::AccessDenied do |exception|
   	redirect_to '/login', :alert => exception.message
+  end
+
+  def store_location
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/login/ || request.fullpath =~ /\/logout/
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || '/'
+  end
+
+  def after_update_path_for(resource)
+    session[:previous_url] || '/'
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || '/'
   end
 end
