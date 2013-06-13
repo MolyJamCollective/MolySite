cec3658706f40b4f339818558b95aab51f9c22cd
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-06-13 00:57:51 -0400

    fix controller errors

--- app/controllers/mail_forwarder_controller.rb
+++ app/controllers/mail_forwarder_controller.rb
@@ -1,11 +1,12 @@
 class MailForwarderController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  skip_authorize_resource
+  skip_authorization_check
 
   def create
   	if params[:mail]
   	  MailForwarder.receive(params[:mail])
   	end
+  	render nothing: true
   end
 
 end
--- app/mailers/mail_forwarder.rb
+++ app/mailers/mail_forwarder.rb
@@ -1,8 +1,6 @@
 class MailForwarder < ActionMailer::Base
 
   def receive(email)
-    puts email
-
-    #mail(subject: )
+  
   end
 end
