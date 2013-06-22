7691f2e950a5f3e12c5d23466d8332532a8cc27e
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-06-21 20:10:43 -0400

    update mail_receiver to reflect the needs of the production environment

--- lib/mail_receiver.rb
+++ lib/mail_receiver.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 require 'cgi'
 
-`curl -d "message=#{CGI.escape(STDIN.read)}" http://localhost/mail_forwarder`
+`curl -d "message=#{CGI.escape(STDIN.read)}" http://www.molyjam.com/mail_forwarder`
