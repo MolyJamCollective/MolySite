0b57d0c48e2deb0cff09cb749b18688e9bf29b8a
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-06-28 23:08:50 -0400

    restructor user abilties

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,44 +4,47 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    webmasters = user.group?(:Webmasters)
-    organizers = user.group?(:Organizers) 
-    hosts      = user.group?(:Hosts)
-    jammers    = user.group?(:Jammers)
-    users      = user.group?(:Users)
-
-    # Want Cascading 'C' Style Switch Statment
-    if(webmasters)
+    user_groups = {
+      webmasters: user.group?(:Webmasters),
+      organizers: user.group?(:Organizers),
+      hosts:      user.group?(:Hosts),
+      jammers:    user.group?(:Jammers),
+      users:      user.group?(:Users)
+    }
+ 
+    if user_groups[:organizers] || user_groups[:webmasters] 
+      can :manage, :all 
+      can [:approve, :show], Venue
     end
 
-    if(organizers || webmasters)
-      can :manage, :all
-      can :approve, Venue
-      can :show, Venue
-    end
-
-    if(hosts || organizers || webmasters)
-      user.groups.each do |group|
-        can [:read, :update, :destroy], Venue, group_id: group.id
-        can :read, group
-        can :manage, Membership, group_id: group.id
-        cannot :destroy, Membership, role: Membership::ROLES[:founder]
-      end
-
+    if(user_groups[:hosts] || user_groups[:organizers] || user_groups[:webmasters])
       can [:manage, :move_up, :move_down], Sponsor
       can :manage, Attachment
       can :create, Venue
       can :host_resources, :page
     end
 
-    if(jammers || hosts || organizers || webmasters)
-      can :manage, UserFileUpload, user_id: user.id
-    end
-
-    if(users || jammers || hosts || organizers || webmasters)
+    if(user_groups[:users] || user_groups[:jammers] || user_groups[:hosts] || user_groups[:organizers] || user_groups[:webmasters])
       can :dashboard, :page
       can :manage, User, id: user.id
       can :create, Game
+    end
+
+    user.groups.each do |group|
+     
+      if group.memberships.where(user_id: user.id).first.role >= Membership::ROLES[:officer]
+        can [:read, :update], Venue, group_id: group.id 
+        can [:read, :update], Game, group_id: group.id
+        can :read, Group, id: group.id
+
+        if group.memberships.where(user_id: user.id).first.role >= Membership::ROLES[:founder]
+          can :destroy, Venue, group_id: group.id
+          can :destroy, Game, group_id: group.id 
+          can :manage, Membership, group_id: group.id
+          cannot :destroy, Membership, role: Membership::ROLES[:founder]
+        end
+      end
+      
     end
 
     can :show_current, Event
