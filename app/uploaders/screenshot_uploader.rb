1ad1d7e232c7df24c91ea91e7e242708b33cceae
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-07-03 08:23:29 -0400

    create carrierwave uploader

---
+++ app/uploaders/screenshot_uploader.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+class ScreenshotUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  version :thumb_grid do
+    process scale: [50, 50]
+  end
+
+  version :thumb_article do
+    process scale: [50, 50]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
